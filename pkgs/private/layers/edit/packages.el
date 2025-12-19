62016c73467430c44fc928b64b84feec1a984b92
NoahStoryM <noahstorym@gmail.com>
2025-12-19 13:49:49 +0800

    [edit] add `wgrep`

--- pkgs/private/layers/edit/packages.el
+++ pkgs/private/layers/edit/packages.el
@@ -2,6 +2,7 @@
 
 (meta-import (private packages avy)
              (private packages consult)
-             (private packages embark))
+             (private packages embark)
+             (private packages wgrep))
 
 (meta-export (private layers edit packages))
---
+++ pkgs/private/packages/wgrep/main.el
@@ -0,0 +1,9 @@
+;; -*- lexical-binding: t -*-
+
+(use-package wgrep
+  :ensure t
+  :defer t
+  :custom
+  (wgrep-auto-save-buffer t))
+
+(meta-export (private packages wgrep))
