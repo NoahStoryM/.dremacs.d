b08235975f28b2431a212e3c5f9ba7aac2e1daa7
NoahStoryM <noahstorym@gmail.com>
2025-12-20 17:22:36 +0800

    [default] update

--- pkgs/private/layers/default/keybindings.el
+++ pkgs/private/layers/default/keybindings.el
@@ -1,11 +1,9 @@
 ;; -*- lexical-binding: t -*-
 
 (use-package emacs
-  :config
-  (with-selected-frame (selected-frame)
-    (keymap-set input-decode-map "C-i" [C-i])
-    (keymap-set input-decode-map "C-m" [C-m])
-    (keymap-set input-decode-map "C-[" [C-bracketleft]))
+  :init
+  (keymap-set input-decode-map "C-i" "H-i")
+  (keymap-set input-decode-map "C-m" "H-m")
   :bind
   ;; Make TAB in minibuffer behave like shell completion
   (:map minibuffer-mode-map ("TAB" . minibuffer-complete)))
--- pkgs/private/main.el
+++ pkgs/private/main.el
@@ -2,13 +2,13 @@
 
 (meta-import (private layers default)
              (private layers theme)
-             (private layers meow)
              (private layers edit)
              (private layers completion)
              (private layers term)
              (private layers git)
              (private layers markdown)
              (private layers data)
-             (private layers template))
+             (private layers template)
+             (private layers meow))
 
 (meta-export (private))
