1cee2fc773ab294df1a6ca3de92be516321a4a6d
NoahStoryM <noahstorym@gmail.com>
2025-12-22 14:23:24 +0800

    [meow] using the official VIM-style QWERTY layout

--- pkgs/private/layers/default/keybindings.el
+++ pkgs/private/layers/default/keybindings.el
@@ -1,12 +1,6 @@
 ;; -*- lexical-binding: t -*-
 
 (use-package emacs
-  :init
-  (keymap-set input-decode-map "C-i" "H-i")
-  (keymap-set input-decode-map "C-m" "H-m")
-  ;; :config
-  ;; (cua-mode)
-
   :bind
   ;; Make TAB in minibuffer behave like shell completion
   (:map minibuffer-mode-map ("TAB" . minibuffer-complete)))
--- pkgs/private/layers/meow/main.el
+++ pkgs/private/layers/meow/main.el
@@ -1,6 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 (meta-import (private layers meow packages)
-             (private layers meow config))
+             (private layers meow config)
+             (private layers meow keybindings))
 
 (meta-export (private layers meow))
