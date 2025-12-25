8c742672bbd3abd844447097005961a830d1438a
NoahStoryM <noahstorym@gmail.com>
2025-12-25 20:48:04 +0800

    `default`: update some keybindings

--- pkgs/private/layers/default/keybindings.el
+++ pkgs/private/layers/default/keybindings.el
@@ -3,7 +3,27 @@
 (use-package emacs
   :bind
   (([remap list-buffers] . ibuffer))
-  (:map minibuffer-mode-map ("<tab>" . minibuffer-complete)))
+  (:map
+   minibuffer-mode-map
+   ("<tab>" . minibuffer-complete)))
+
+(use-package dired
+  :bind
+  (:map
+   dired-mode-map
+   ("j" . dired-previous-line)
+   ("l" . dired-next-line)
+   ("n" . dired-goto-file)
+   ("p" . dired-do-redisplay)))
+
+(use-package ibuffer
+  :bind
+  (:map
+   ibuffer-mode-map
+   ("j" . ibuffer-forward-line)
+   ("l" . ibuffer-backward-line)
+   ("n" . ibuffer-jump-to-buffer)
+   ("p" . ibuffer-do-redisplay)))
 
 (use-package mule-cmds
   :bind
