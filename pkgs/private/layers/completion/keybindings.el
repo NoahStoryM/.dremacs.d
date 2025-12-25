be90e19777ef51438942f51410a9b0cd0665482f
NoahStoryM <noahstorym@gmail.com>
2025-12-25 20:17:28 +0800

    `completion`: update keybindings

--- pkgs/private/layers/completion/keybindings.el
+++ pkgs/private/layers/completion/keybindings.el
@@ -2,9 +2,25 @@
 
 (meta-import (private layers completion packages))
 
+(use-package corfu
+  :bind
+  (:map
+   corfu-map
+   ("C-j" . corfu-previous)
+   ("C-l" . corfu-next)
+   ("C-k" . keyboard-quit)))
+
+(use-package vertico
+  :bind
+  (:map
+   vertico-map
+   ("C-j" . vertico-previous)
+   ("C-l" . vertico-next)))
+
 (use-package vertico-directory
   :bind
-  (:map vertico-map
-        ("M-DEL" . vertico-directory-delete-word)))
+  (:map
+   vertico-map
+   ("C-k" . vertico-directory-delete-word)))
 
 (meta-export (private layers completion keybindings))
