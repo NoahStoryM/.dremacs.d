53451bc5b5481bf69da4eded98b9860cedd86372
NoahStoryM <noahstorym@gmail.com>
2025-12-27 13:48:56 +0800

    `dir`: add some keybindings

--- pkgs/private/layers/dir/keybindings.el
+++ pkgs/private/layers/dir/keybindings.el
@@ -6,6 +6,19 @@
   :bind
   (:map
    dirvish-mode-map
+   ("?" . dirvish-dispatch)          ; [?] a helpful cheatsheet
+   ("a" . dirvish-setup-menu)        ; [a]ttributes settings:`t' toggles mtime, `f' toggles fullframe, etc.
+   ("f" . dirvish-file-info-menu)    ; [f]ile info
+   ("o" . dirvish-quick-access)      ; [o]pen `dirvish-quick-access-entries'
+   ("s" . dirvish-quicksort)         ; [s]ort flie list
+   ("r" . dirvish-history-jump)      ; [r]ecent visited
+   (";" . dirvish-ls-switches-menu)
+   ("y" . dirvish-vc-menu)
+   ("*" . dirvish-mark-menu)
+   ("v" . dirvish-yank-menu)
+   ("N" . dirvish-narrow)
+   ("'" . dirvish-history-last)
+   ("i" . dirvish-subtree-toggle)
    ("M-f" . dirvish-history-go-forward)
    ("M-b" . dirvish-history-go-backward)
    ("M-e" . dirvish-emerge-menu)
