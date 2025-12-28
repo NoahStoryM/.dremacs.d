ab0d5f34b84d78de8dd5318a83b0420eb3d15e1e
NoahStoryM <noahstorym@gmail.com>
2025-12-28 14:05:09 +0800

    `term`: add some keybindings to eshell

--- pkgs/private/layers/term/keybindings.el
+++ pkgs/private/layers/term/keybindings.el
@@ -2,9 +2,17 @@
 
 (meta-import (private layers term packages))
 
+(use-package eshell
+  :bind
+  (:map
+   eshell-mode-map
+   ("C-j" . eshell-previous-input)
+   ("C-l" . eshell-next-input)))
+
 (use-package capf-autosuggest
   :bind
-  (:map capf-autosuggest-active-mode-map
-        ("<tab>" . capf-autosuggest-accept)))
+  (:map
+   capf-autosuggest-active-mode-map
+   ("<tab>" . capf-autosuggest-accept)))
 
 (meta-export (private layers term keybindings))
