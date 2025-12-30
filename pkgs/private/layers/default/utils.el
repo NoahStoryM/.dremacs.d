3691db5b8308910e7276b59e1dc1af8cc8f1324d
NoahStoryM <noahstorym@gmail.com>
2025-12-30 12:01:51 +0800

    Fix `private--backup-file-name`

--- pkgs/private/layers/default/utils.el
+++ pkgs/private/layers/default/utils.el
@@ -3,10 +3,10 @@
 (defun private--backup-file-name (fpath)
   "Return a new file path of a given file path.
 If the new path's directories do not exist, create them."
-  (let* ((backup-root-dir (file-name-concat user-emacs-directory "emacs-backup"))
-         (file-path (replace-regexp-in-string "[A-Za-z]:" "" fpath)) ; Remove Windows driver letter
-         (backup-file-path (replace-regexp-in-string "//" "/" (concat backup-root-dir file-path "~"))))
-    (make-directory (file-name-directory backup-file-path) t)
-    backup-file-path))
+  (let* ((backupRootDir (concat user-emacs-directory "emacs-backup/"))
+         (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path
+         (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") )))
+    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+    backupFilePath))
 
 (meta-export (private layers default utils))
