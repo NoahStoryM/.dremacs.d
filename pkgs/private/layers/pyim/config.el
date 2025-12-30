f137db270a70f3328e64b440fffc22b9c94bdf4c
NoahStoryM <noahstorym@gmail.com>
2025-12-30 20:19:43 +0800

    `pyim`: use `:defer t`

--- pkgs/private/layers/pyim/config.el
+++ pkgs/private/layers/pyim/config.el
@@ -3,10 +3,12 @@
 (meta-import (private layers pyim packages))
 
 (use-package pyim
+  :defer t
   :custom
   (default-input-method "pyim"))
 
 (use-package pyim-basedict
+  :defer t
   :config
   (pyim-basedict-enable))
 
