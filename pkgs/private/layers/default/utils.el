cd71d532eae7accd4e6411375195828e17317ccf
NoahStoryM <noahstorym@gmail.com>
2025-12-30 21:59:31 +0800

    `default`: update `private--backup-file-name`

--- pkgs/private/layers/default/utils.el
+++ pkgs/private/layers/default/utils.el
@@ -5,8 +5,9 @@
 If the new path's directories do not exist, create them."
   (let* ((backupRootDir (concat user-emacs-directory "emacs-backup/"))
          (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path
-         (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") )))
-    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+         (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") ))
+         (backupDirPath (file-name-directory backupFilePath)))
+    (make-directory backupDirPath backupDirPath)
     backupFilePath))
 
 (meta-export (private layers default utils))
