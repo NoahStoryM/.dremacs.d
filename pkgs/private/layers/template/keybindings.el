85997d2d36dfdce94f36b09597d59a7846b013e6
NoahStoryM <noahstorym@gmail.com>
2026-01-09 15:26:20 +0800

    `template`: update keybindings

--- pkgs/private/layers/template/keybindings.el
+++ pkgs/private/layers/template/keybindings.el
@@ -3,13 +3,12 @@
 (meta-import (private layers template packages))
 
 (use-package tempel
-  :bind (("M-*" . tempel-insert)
-         ("M-+" . tempel-complete)
-         :map tempel-map
-         ("C-c RET" . tempel-done)
-         ("C-<down>" . tempel-next)
-         ("C-<up>" . tempel-previous)
-         ("M-<down>" . tempel-next)
-         ("M-<up>" . tempel-previous)))
+  :bind
+  ("C-k" . tempel-insert)
+  (:map
+   tempel-map
+   ("C-k" . tempel-done)
+   ("C-l" . tempel-next)
+   ("C-j" . tempel-previous)))
 
 (meta-export (private layers template keybindings))
