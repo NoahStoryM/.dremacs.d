c4527d6a93b979612732eda8134526a6a6cbe89e
NoahStoryM <noahstorym@gmail.com>
2026-01-10 16:47:14 +0800

    `meow`: avoid redefine `meow--read-cursor-face-color`

--- pkgs/private/packages/meow/main.el
+++ pkgs/private/packages/meow/main.el
@@ -4,26 +4,13 @@
   :ensure t
   :defer t
   :custom-face
-  (meow-normal-cursor ((t (:inherit success))))
-  (meow-insert-cursor ((t (:inherit font-lock-keyword-face))))
-  (meow-motion-cursor ((t (:inherit font-lock-doc-face))))
+  (meow-normal-cursor ((t (:inherit unspecified :background ,(face-attribute 'success :foreground)))))
+  (meow-insert-cursor ((t (:inherit unspecified :background ,(face-attribute 'font-lock-keyword-face :foreground)))))
+  (meow-motion-cursor ((t (:inherit unspecified :background ,(face-attribute 'font-lock-doc-face :foreground)))))
   :custom
   (meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
   (meow-use-clipboard t)
   :config
-  (defun meow--read-cursor-face-color (face)
-    "Read cursor color from face."
-    (if (memq face '(meow-normal-cursor
-                     meow-insert-cursor
-                     meow-motion-cursor))
-        (face-foreground face nil t)
-      (let ((f (face-attribute face :inherit)))
-        (if (equal 'unspecified f)
-            (let ((color (face-attribute face :background)))
-              (if (equal 'unspecified color)
-                  (face-attribute 'default :foreground)
-                color))
-          (meow--read-cursor-face-color f)))))
   (setq meow-cursor-type-normal 'hollow)
   (setq meow-cursor-type-motion 'hollow)
   (setq meow-cursor-type-keypad 'box))
