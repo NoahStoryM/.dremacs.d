c190210daeac9dcb37714f03c1c9d92881f11a25
NoahStoryM <noahstorym@gmail.com>
2026-01-10 17:52:30 +0800

    Update

--- early-init.el
+++ early-init.el
@@ -4,11 +4,14 @@
 (setopt gc-cons-threshold #x8000000)
 (setopt inhibit-startup-echo-area-message (user-login-name))
 (setopt frame-resize-pixelwise t)
+(setopt default-frame-alist
+        `((foreground-color . "#002b36")
+          (background-color . "#fdf6e3")
+          (undecorated . t)
+          .
+          ,default-frame-alist))
 (set-scroll-bar-mode nil)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (pixel-scroll-precision-mode 1)
-(add-to-list 'default-frame-alist '(foreground-color . "#002b36"))
-(add-to-list 'default-frame-alist '(background-color . "#fdf6e3"))
-(add-to-list 'default-frame-alist '(undecorated . t))
